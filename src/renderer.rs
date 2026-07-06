48388e713bac2d5944f8ddca01868333e7d0c7d8
Lucas Galante <lsgalante12@gmail.com>
2026-07-05 20:04:33 -0400

    Clear ui_context active popovers at the start of rebuild_layout

--- src/renderer.rs
+++ src/renderer.rs
@@ -45,6 +45,7 @@ impl SystemInterface {
 
         cce_ui::widget::hover_animation::reset_frame_registration();
         cce_ui::widget::popovers::clear();
+        self.ui_context.clear_popovers();
         cce_ui::widget::hover_animation::set_scroll_offset(self.scroll_y);
         cce_ui::widget::hover_animation::set_cursor_pos(self.cursor_x, self.cursor_y);
 
